4b76fca6d0ce26f4335b0449197717b1d5db9caf
Stephen Cleary <none@example.com>
2013-11-01 17:59:07 -0400

    Metadata

--- src/Nito.AsyncEx.AsyncDiagnostics/Properties/AssemblyInfo.cs
+++ src/Nito.AsyncEx.AsyncDiagnostics/Properties/AssemblyInfo.cs
@@ -1,36 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Nito.AsyncEx.PostSharp (NET45)")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nito.AsyncEx.PostSharp (NET45)")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("AsyncDiagnostics")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Easy-to-use exception causality chains for async/await.")]
+[assembly: AssemblyCompany("Stephen Cleary")]
+[assembly: AssemblyProduct("Nito.AsyncEx.AsyncDiagnostics")]
+[assembly: AssemblyCopyright("Copyright © 2013 Stephen Cleary")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e4ed5605-c20e-4501-abf1-1df6084cf187")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
